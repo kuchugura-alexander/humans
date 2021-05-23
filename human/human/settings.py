53ba2b80f269ff9fb6381a7d7d9c7bf39e02d162
whitedemon <whitedemonreg@gmail.com>
2021-05-23 19:40:13 +0300

    [+] settings.py.

--- human/human/settings.py
+++ human/human/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'files', 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'files', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # BASE_DIR / "static",
